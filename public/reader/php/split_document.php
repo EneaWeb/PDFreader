69e87d5f447ebf13b72e28eea3e403eff51e5a47
EneaWeb <giovannicweb@gmail.com>
2016-09-13 10:38:13 +0200

    :

--- public/reader/php/split_document.php
+++ public/reader/php/split_document.php
@@ -5,9 +5,9 @@ require_once("lib/common.php");
 
 $configManager = new Config();
 if($configManager->getConfig('admin.password')==null){
-	$url = 'setup.php';
-	header("Location: $url");
-	exit;
+    $url = 'setup.php';
+    header("Location: $url");
+    exit;
 }
 ?>
 <!doctype html>
@@ -16,77 +16,85 @@ if($configManager->getConfig('admin.password')==null){
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width" />
         <style type="text/css" media="screen">
-			html, body	{ height:100%; }
-			body { margin:0; padding:0; overflow:auto; }
-			#flashContent { display:none; }
+            html, body  { height:100%; }
+            body { margin:0; padding:0; overflow:auto; }
+            #flashContent { display:none; }
         </style>
 
-		<link rel="stylesheet" type="text/css" href="css/flowpaper.css" />
-		<script type="text/javascript" src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/jquery.extensions.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/flowpaper.css" />
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.extensions.min.js"></script>
         <!--[if gte IE 10 | !IE ]><!-->
         <script type="text/javascript" src="js/three.min.js"></script>
         <!--<![endif]-->
-		<script type="text/javascript" src="js/flowpaper.js"></script>
-		<script type="text/javascript" src="js/flowpaper_handlers.js"></script>
+        <script type="text/javascript" src="js/flowpaper.js"></script>
+        <script type="text/javascript" src="js/flowpaper_handlers.js"></script>
     </head>
     <body>
-			<div id="documentViewer" class="flowpaper_viewer" style="position:absolute;left:10px;top:10px;width:770px;height:500px"></div>
-	        <?php
-	        if(isset($_GET["doc"])){
-	            $doc = substr($_GET["doc"],0,strlen($_GET["doc"])-4);
-	        }else{
-	            $doc = "Report";
-	        }
+            <div id="documentViewer" class="flowpaper_viewer" style="position:absolute;left:10px;top:10px;width:770px;height:500px"></div>
+            <?php
+            if(isset($_GET["doc"])){
+                $doc = substr($_GET["doc"],0,strlen($_GET["doc"])-4);
+            }else{
+                $doc = "Report";
+            }
 
-			$pdfFilePath = $configManager->getConfig('path.pdf') . $_GET["subfolder"];
-			?>
-	        <script type="text/javascript">
-		        function getDocumentUrl(document){
-		        	var numPages 			= <?php echo getTotalPages($pdfFilePath . $doc . ".pdf") ?>;
-					var url = "{services/view.php?doc={doc}&format={format}&subfolder=<?php echo $_GET["subfolder"] ?>&page=[*,0],{numPages}}";
-						url = url.replace("{doc}",document);
-						url = url.replace("{numPages}",numPages);
-						return url;
-		        }
+            $pdfFilePath = $configManager->getConfig('path.pdf') . $_GET["subfolder"];
+            ?>
+            <script type="text/javascript">
+                function getDocumentUrl(document){
+                    var numPages            = <?php echo getTotalPages($pdfFilePath . $doc . ".pdf") ?>;
+                    var url = "{services/view.php?doc={doc}&format={format}&subfolder=<?php echo $_GET["subfolder"] ?>&page=[*,0],{numPages}}";
+                        url = url.replace("{doc}",document);
+                        url = url.replace("{numPages}",numPages);
+                        return url;
+                }
 
-				var searchServiceUrl	= escape('services/containstext.php?doc=<?php echo $doc ?>&page=[page]&searchterm=[searchterm]');
-				$('#documentViewer').FlowPaperViewer(
-				  { config : {
+                var searchServiceUrl    = escape('services/containstext.php?doc=<?php echo $doc ?>&page=[page]&searchterm=[searchterm]');
+                $('#documentViewer').FlowPaperViewer(
+                  { config : {
 
-						 DOC : escape(getDocumentUrl("<?php echo $doc ?>")),
-						 Scale : 0.6,
-						 ZoomTransition : 'easeOut',
-						 ZoomTime : 0.5,
-						 ZoomInterval : 0.1,
-						 FitPageOnLoad : true,
-						 FitWidthOnLoad : false,
-						 FullScreenAsMaxWindow : false,
-						 ProgressiveLoading : false,
-						 MinZoomSize : 0.2,
-						 MaxZoomSize : 5,
-						 SearchMatchAll : false,
-  						 SearchServiceUrl : searchServiceUrl,
-						 InitViewMode : '',
-						 EnableWebGL : true,
-						 MixedMode : true,
-						 RenderingOrder : '<?php echo ($configManager->getConfig('renderingorder.primary') . ',' . $configManager->getConfig('renderingorder.secondary')) ?>',
+                        DOC : escape(getDocumentUrl("<?php echo $doc ?>")),
+                        Scale : 0.6,
+                        ZoomTransition : 'easeOut',
+                        ZoomTime : 0.5,
+                        ZoomInterval : 0.1,
+                        FitPageOnLoad : true,
+                        FitWidthOnLoad : false,
+                        FullScreenAsMaxWindow : false,
+                        ProgressiveLoading : false,
+                        MinZoomSize : 0.2,
+                        MaxZoomSize : 5,
+                        SearchMatchAll : false,
+                        SearchServiceUrl : searchServiceUrl,
+                        InitViewMode : '',
+                        EnableWebGL : true,
+                        MixedMode : true,
+                        RenderingOrder : '<?php echo ($configManager->getConfig('renderingorder.primary') . ',' . $configManager->getConfig('renderingorder.secondary')) ?>',
 
-						 ViewModeToolsVisible : true,
-						 ZoomToolsVisible : true,
-						 NavToolsVisible : true,
-						 CursorToolsVisible : true,
-						 SearchToolsVisible : true,
-  						 key : '<?php echo $configManager->getConfig('licensekey') ?>',
-
-  						 DocSizeQueryService : 'services/swfsize.php?doc=<?php echo $doc ?>',
-
-						 JSONDataType : 'jsonp',
-
-						 WMode : 'transparent',
-  						 localeChain: 'en_US'
-						 }}
-				);
-	        </script>
+                        ViewModeToolsVisible : true,
+                        ZoomToolsVisible : true,
+                        NavToolsVisible : true,
+                        CursorToolsVisible : true,
+                        SearchToolsVisible : true,
+                        SidebarOpen         : false,
+                        ReflowWithSidebar   : false,
+                        SearchHinter        : null,
+                        AnimationStyle      : 'Flip',
+                        AddPageShadows      : true,
+                        Animate             : true,
+                        SingleFirstPage     : true,
+                        StartAt             : -1,
+                        MaxVisiblePages     : 2,
+                        LayoutMode          : 'Responsive',
+                        InactiveOpacity     : '0.3',
+                        BackgroundColor     : '#cccccc',
+                        BackgroundImage     : '{{BackgroundImage}}',
+                        AddDropShadow       : true,
+                        AnimationClasses    : { H1Text : 'Fade In Up',H2Text : 'None',H3Text : 'None',H4Text : 'Alternating Lines', BodyText : 'None' },
+                        key                 : '~13ba6daaa1c004281b9'
+                    }}
+                );
+            </script>
    </body>
 </html>
