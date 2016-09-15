745d23cb95fb5e85043e0a41e4f39c6b55c6c2fc
EneaWeb <giovannicweb@gmail.com>
2016-09-15 10:14:53 +0200

    .

--- public/reader/php/split_document.php
+++ public/reader/php/split_document.php
@@ -16,48 +16,9 @@ if($configManager->getConfig('admin.password')==null){
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width" />
         <style type="text/css" media="screen">
-            html, body  { width:100%; height:100%; }
+            html, body  { height:100%; }
             body { margin:0; padding:0; overflow:auto; }
-
-            /* LEFT CLICK TO SCROLL PAGES */
-            .flowpaper_fisheye_panelLeft {
-                background-color:inherit!important;
-            }
-            .flowpaper_fisheye_leftArrow {
-                top: 20%!important;
-                right: 10px!important;
-                border-bottom: 16px solid transparent!important;
-                border-top: 20px solid transparent!important;
-            }
-            /* RIGHT CLICK TO SCROLL PAGES */
-            .flowpaper_fisheye_panelRight {
-                background-color:inherit!important;
-            }
-            .flowpaper_fisheye_rightArrow {
-                top: 20%!important;
-                left: 10px!important;
-                border-bottom: 16px solid transparent!important;
-                border-top: 20px solid transparent!important;
-            }
-
-            #pagesContainer_documentViewer_panelRight, 
-            #pagesContainer_documentViewer_panelLeft,
-            #toolbar_documentViewer {
-                background-color:black!important;
-            }
-
-            #toolbar_documentViewer_barSearchTools.flowpaper_tbseparator {
-                display:none!important;
-            }
-
-            .flowpaper_tbbutton.flowpaper_bttnTextSelect {
-                display:none!important;
-            }
-            .flowpaper_bttnDownload.flowpaper_tbbutton.download {
-                display:none!important;
-            }
-
-            .flowpaper_fisheye_item { cursor:pointer!important; }
+            #flashContent { display:none; }
         </style>
 
         <link rel="stylesheet" type="text/css" href="css/flowpaper.css" />
@@ -67,10 +28,10 @@ if($configManager->getConfig('admin.password')==null){
         <script type="text/javascript" src="js/three.min.js"></script>
         <!--<![endif]-->
         <script type="text/javascript" src="js/flowpaper.js"></script>
-        <!-- <script type="text/javascript" src="js/flowpaper_handlers.js"></script> -->
+        <script type="text/javascript" src="js/flowpaper_handlers.js"></script>
     </head>
     <body>
-            <div id="documentViewer" class="flowpaper_viewer" style="position:absolute;left:0;top:0;width:100%;height:100%"></div>
+            <div id="documentViewer" class="flowpaper_viewer" style="position:absolute;left:10px;top:10px;width:770px;height:500px"></div>
             <?php
             if(isset($_GET["doc"])){
                 $doc = substr($_GET["doc"],0,strlen($_GET["doc"])-4);
@@ -93,47 +54,38 @@ if($configManager->getConfig('admin.password')==null){
                 $('#documentViewer').FlowPaperViewer(
                   { config : {
 
-                        DOC : escape(getDocumentUrl("<?php echo $doc ?>")),
-                        Scale : 0.6,
-                        ZoomTransition : 'easeOut',
-                        ZoomTime : 0.5,
-                        ZoomInterval : 0.1,
-                        FitPageOnLoad : true,
-                        FitWidthOnLoad : false,
-                        FullScreenAsMaxWindow : false,
-                        ProgressiveLoading : false,
-                        MinZoomSize : 0.2,
-                        MaxZoomSize : 5,
-                        SearchMatchAll : false,
-                        SearchServiceUrl : searchServiceUrl,
-                        InitViewMode : '',
-                        EnableWebGL : true,
-                        MixedMode : true,
-                        RenderingOrder : '<?php echo ($configManager->getConfig('renderingorder.primary') . ',' . $configManager->getConfig('renderingorder.secondary')) ?>',
+                         DOC : escape(getDocumentUrl("<?php echo $doc ?>")),
+                         Scale : 0.6,
+                         ZoomTransition : 'easeOut',
+                         ZoomTime : 0.5,
+                         ZoomInterval : 0.1,
+                         FitPageOnLoad : true,
+                         FitWidthOnLoad : false,
+                         FullScreenAsMaxWindow : false,
+                         ProgressiveLoading : false,
+                         MinZoomSize : 0.2,
+                         MaxZoomSize : 5,
+                         SearchMatchAll : false,
+                         SearchServiceUrl : searchServiceUrl,
+                         InitViewMode : '',
+                         EnableWebGL : true,
+                         MixedMode : true,
+                         RenderingOrder : '<?php echo ($configManager->getConfig('renderingorder.primary') . ',' . $configManager->getConfig('renderingorder.secondary')) ?>',
 
-                        ViewModeToolsVisible : true,
-                        ZoomToolsVisible : true,
-                        NavToolsVisible : true,
-                        CursorToolsVisible : true,
-                        SearchToolsVisible : true,
-                        SidebarOpen         : false,
-                        ReflowWithSidebar   : false,
-                        SearchHinter        : null,
-                        AnimationStyle      : 'FlipView',
-                        AddPageShadows      : true,
-                        Animate             : true,
-                        SingleFirstPage     : true,
-                        StartAt             : -1,
-                        MaxVisiblePages     : 2,
-                        LayoutMode          : 'Responsive',
-                        InactiveOpacity     : '0.3',
-                        BackgroundColor     : '#2A2A2A',
-                        BackgroundImage     : '{{BackgroundImage}}',
-                        AddDropShadow       : true,
-                        localeChain: 'it_IT',
-                        AnimationClasses    : { H1Text : 'Fade In Up',H2Text : 'None',H3Text : 'None',H4Text : 'Alternating Lines', BodyText : 'None' },
-                        key : '<?php echo $configManager->getConfig('licensekey') ?>',
-                    }}
+                         ViewModeToolsVisible : true,
+                         ZoomToolsVisible : true,
+                         NavToolsVisible : true,
+                         CursorToolsVisible : true,
+                         SearchToolsVisible : true,
+                         key : '<?php echo $configManager->getConfig('licensekey') ?>',
+
+                         DocSizeQueryService : 'services/swfsize.php?doc=<?php echo $doc ?>',
+
+                         JSONDataType : 'jsonp',
+
+                         WMode : 'transparent',
+                         localeChain: 'en_US'
+                         }}
                 );
             </script>
    </body>
