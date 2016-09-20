7cd0c4a6e1f4fad3413b65bbffe9b7883f0857f0
EneaWeb <giovannicweb@gmail.com>
2016-09-20 13:48:00 +0200

    .

--- index.php
+++ index.php
@@ -1,30 +1,17 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
+try {
+    $dbh = new PDO('mysql:host=46.37.12.19;dbname=admin_spreanewdb', 'admin_shopnew', 'nGY96@l8');
+    foreach($dbh->query('SELECT * from FOO') as $row) {
+        print_r($row);
     }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+$id_rivista = $_GET['id_rivista'];
+$num_rivista = $_GET['num_rivista'];
 
-session_start();
 
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
-$app = new \Slim\App($settings);
-
-// Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
-
-// Register middleware
-require __DIR__ . '/../src/middleware.php';
-
-// Register routes
-require __DIR__ . '/../src/routes.php';
-
-// Run app
-$app->run();
+if ($_GET['id_rivsta'])
