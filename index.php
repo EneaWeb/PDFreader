93bf1bb12634d739e59bfda1da29102be756fe12
EneaWeb <giovannicweb@gmail.com>
2016-09-20 13:57:05 +0200

    .

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
+$id_rivista = isset($_GET['id_rivista']) ? $_GET['id_rivista'] : '';
+$num_rivista = isset($_GET['num_rivista']) ? $_GET['num_rivista'] : '';
+
+## 
+## ERROR 0009
+##
+if ($id_rivista == '' || $num_rivista == '') {
+	echo 'Errore 0009. Valori inesistenti.';
+	exit();
+}
+##
+
 try {
     $dbh = new PDO('mysql:host=46.37.12.19;dbname=admin_spreanewdb', 'admin_shopnew', 'nGY96@l8');
-    foreach($dbh->query('SELECT * from FOO') as $row) {
+    foreach($dbh->query('SELECT * from pdfs WHERE id_rivista = $id_rivista AND num_rivista = $num_rivista') as $row) {
         print_r($row);
     }
     $dbh = null;
@@ -9,7 +21,3 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-
-$id_rivista = $_GET['id_rivista'];
-$num_rivista = $_GET['num_rivista'];
-
