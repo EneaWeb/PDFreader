f70d6cb4b52302053f5a71b4b93de92778f2894b
EneaWeb <giovannicweb@gmail.com>
2016-09-21 13:48:44 +0200

    :

--- r/php/index.php
+++ r/php/index.php
@@ -215,7 +215,7 @@ $(function() {
 				foreach($dbh->query("SELECT * from pdfs") as $row) {
 					?>
 
-					<tr class="unselectedRow folder" onclick="location.href='index.php?subfolder=&doc=<?php echo $row['name'];?>'">
+					<tr class="unselectedRow folder" onclick="location.href='index.php?subfolder=&doc=<?php echo $row["name"];?>'">
 					<td><?php echo $row['name'];?></td><td><?php echo $row['id_rivista'];?></td><td><?php echo $row['num_rivista'];?></td>
 					</tr>
 
