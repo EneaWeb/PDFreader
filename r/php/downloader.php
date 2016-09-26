967626e5ee0ab58d531c734f5f93e4b0233ffd27
Giovanni Cellie <giovannicweb@gmail.com>
2016-09-26 17:23:15 +0200

    :

--- r/php/downloader.php
+++ r/php/downloader.php
@@ -9,14 +9,14 @@ if (isset($_POST['id_anagrafica']) && $_POST['doc'] != '') {
 	$doc = $_POST['doc'];
 	$id_anagrafica = $_POST['id_anagrafica'];
 
-	$file = "/var/www/pdf/r/php/pdf/temp/".$id_anagrafica."-".$doc."";
+	$file = "/media/pdf/r/php/pdf/temp/".$id_anagrafica."-".$doc."";
 
-	$txt = '/var/www/pdf/r/php/pdf/temp/'.$id_anagrafica.'.txt';
-	echo exec("touch ".$txt);
+	$txt = '/media/pdf/r/php/pdf/temp/'.$id_anagrafica.'.txt';
+	echo exec("touch ".$txt.'  2>&1');
 	exec("echo ".$id_anagrafica." >> ".$txt);
 
 	exec("paps --left-margin 1 top-margin 0 ".$id_anagrafica.".txt | ps2pdf -dEPSCrop - ".$id_anagrafica.".pdf");
-	exec("pdftk /var/www/pdf/r/php/pdf/".$doc." stamp /var/www/pdf/r/php/pdf/temp/".$id_anagrafica.".pdf output ".$file."");
+	exec("pdftk /media/pdf/r/php/pdf/".$doc." stamp /media/pdf/r/php/pdf/temp/".$id_anagrafica.".pdf output ".$file."");
 
 	// echo $file;
 } 
