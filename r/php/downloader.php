28b11c1bd869efb9f9fde137ef09177059337e2f
Giovanni Cellie <giovannicweb@gmail.com>
2016-09-26 17:54:22 +0200

    :

--- r/php/downloader.php
+++ r/php/downloader.php
@@ -16,7 +16,7 @@ if (isset($_POST['id_anagrafica']) && $_POST['doc'] != '') {
 	exec("touch ".$txt."; chmod 777 ".$txt);
 	exec("echo ".$id_anagrafica." >> ".$txt);
 	exec("CHMOD 0777 /media/pdf/r/php/pdf/temp/".$id_anagrafica.".txt");
-	exec("paps --left-margin 1 top-margin 0 /media/pdf/r/php/pdf/temp/".$id_anagrafica.".txt | ps2pdf -dEPSCrop - /media/pdf/r/php/pdf/temp/".$id_anagrafica.".pdf");
+	exec("paps --left-margin 1 --top-margin 0 /media/pdf/r/php/pdf/temp/".$id_anagrafica.".txt | ps2pdf -dEPSCrop - /media/pdf/r/php/pdf/temp/".$id_anagrafica.".pdf");
 	exec("pdftk /media/pdf/r/php/pdf/".$doc." stamp /media/pdf/r/php/pdf/temp/".$id_anagrafica.".pdf output ".$file."");
 
 	echo $file;
@@ -24,4 +24,4 @@ if (isset($_POST['id_anagrafica']) && $_POST['doc'] != '') {
 
 exit();
 
-// paps --left-margin 1 top-margin 0 /media/pdf/r/php/pdf/temp/2013007173.txt | ps2pdf -dEPSCrop - /media/pdf/r/php/pdf/temp/2013007173.pdf
+// paps --left-margin 1 --top-margin 0 /media/pdf/r/php/pdf/temp/2013007173.txt | ps2pdf -dEPSCrop - /media/pdf/r/php/pdf/temp/2013007173.pdf
