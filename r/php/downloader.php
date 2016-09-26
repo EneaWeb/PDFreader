799ab335d7a917d21799a26c25edb8928416828f
Giovanni Cellie <giovannicweb@gmail.com>
2016-09-26 17:44:55 +0200

    :

--- r/php/downloader.php
+++ r/php/downloader.php
@@ -15,7 +15,7 @@ if (isset($_POST['id_anagrafica']) && $_POST['doc'] != '') {
 	exec("rm -rf ".$txt);
 	exec("touch ".$txt);
 	exec("echo ".$id_anagrafica." >> ".$txt);
-	exec("CHMOD 777 ".$id_anagrafica.".txt");
+	exec("CHMOD 0777 /media/pdf/r/php/pdf/temp/".$id_anagrafica.".txt");
 	exec("paps --left-margin 1 top-margin 0 /media/pdf/r/php/pdf/temp/".$id_anagrafica.".txt | ps2pdf -dEPSCrop - /media/pdf/r/php/pdf/temp/".$id_anagrafica.".pdf");
 	exec("pdftk /media/pdf/r/php/pdf/".$doc." stamp /media/pdf/r/php/pdf/temp/".$id_anagrafica.".pdf output ".$file."");
 
