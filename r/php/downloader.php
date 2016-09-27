bafcee54a156d6ba71f3eabb060816ee9f7a9d8f
Giovanni Cellie <giovannicweb@gmail.com>
2016-09-27 10:53:38 +0200

    .

--- r/php/downloader.php
+++ r/php/downloader.php
@@ -26,16 +26,11 @@ if (isset($_POST['id_anagrafica']) && $_POST['doc'] != '') {
 	$file_public = 'http://pdf.sprea.it/r/php/pdf/temp/'.$new_filename;
 	//echo $file_public;
 
-	//file_get_contents is standard function
-	$content = file_get_contents($file_public);
-	header('Content-Type: application/pdf');
-	header('Content-Length: '.strlen( $content ));
-	header('Content-disposition: inline; filename="' . $file_public . '"');
-	header('Cache-Control: public, must-revalidate, max-age=0');
-	header('Pragma: public');
-	header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
-	header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-	echo $content;
+	header("Content-type:application/pdf");
+	// It will be called downloaded.pdf
+	header("Content-Disposition:attachment;filename='".$file_public."'");
+	// The PDF source is in original.pdf
+	readfile($file_public);
 
 } 
 
